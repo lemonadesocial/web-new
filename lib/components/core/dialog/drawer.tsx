21d021d7f7a669a6a90850d265bf4263d490fa93
Chris Nguyen <chris.dungnguyen@gmail.com>
2025-03-08 10:53:01 +0700

    feat: update options - rm unused

--- lib/components/core/dialog/drawer.tsx
+++ lib/components/core/dialog/drawer.tsx
@@ -22,11 +22,13 @@
 import clsx from 'clsx';
 import React from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
+import { twMerge } from 'tailwind-merge';
 
 interface DrawerOptions<T> {
   props?: T;
-  width?: string;
   position?: 'left' | 'right';
+  duration?: number;
+  contentClass?: string;
 }
 
 interface Drawer {
@@ -46,13 +48,13 @@ export const drawer: Drawer = {
 export function DrawerContainer() {
   const [isOpen, setIsOpen] = React.useState(false);
   const [drawerContent, setDrawerContent] = React.useState<React.ReactNode>();
-  const [drawerOptions, setDrawerOptions] = React.useState<DrawerOptions<unknown>>({});
+  const [drawerOptions, setDrawerOptions] = React.useState<DrawerOptions<unknown>>({ duration: 0.3 });
   const drawerRef = React.useRef<HTMLDivElement | null>(null);
 
   const openDrawer = React.useCallback(
     <T extends object>(Component: React.ComponentType<T>, options: DrawerOptions<T> = {}) => {
       setDrawerContent(<Component {...(options.props as T)} />);
-      setDrawerOptions(options);
+      setDrawerOptions((prev) => ({ ...prev, ...options }));
       setIsOpen(true);
     },
     [],
@@ -60,7 +62,7 @@ export function DrawerContainer() {
 
   const handleOutsideClick = (event: MouseEvent) => {
     if (drawerRef.current && event.target instanceof Node && drawerRef.current.contains(event.target)) {
-      // ...
+      // do nothing here
     } else {
       setIsOpen(false);
     }
@@ -96,12 +98,11 @@ export function DrawerContainer() {
                 initial={{ x: '100%' }}
                 animate={{ x: 0 }}
                 exit={{ x: '100%' }}
-                transition={{ duration: 0.3 }}
+                transition={{ duration: drawerOptions.duration }}
                 ref={drawerRef}
-                className=" rounded-sm bg-primary-500 flex-1 max-w-[528px]"
+                className={twMerge('rounded-sm flex-1 max-w-[528px]', drawerOptions.contentClass)}
               >
                 {drawerContent}
-                <button onClick={() => setIsOpen(false)}>close</button>
               </motion.div>
             </div>
           </div>
