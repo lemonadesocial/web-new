58be5eb2423ef2778a02ae6729f9d95949b5ce91
Nam Nguyen <namhoang.td09@gmail.com>
2025-03-27 11:48:40 +0700

    fix: asset prefix

--- lib/utils/constants.ts
+++ lib/utils/constants.ts
@@ -2,5 +2,5 @@ export const GRAPHQL_URL = process.env.NEXT_PUBLIC_GRAPHQL_URL as string;
 export const LEMONADE_DOMAIN = process.env.NEXT_PUBLIC_LEMONADE_DOMAIN as string;
 export const HYDRA_PUBLIC_URL = process.env.NEXT_PUBLIC_HYDRA_PUBLIC_URL as string;
 export const GOOGLE_MAP_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAP_KEY as string;
-export const ASSET_PREFIX = process.env.NEXT_PUBLIC_ASSET_PREFIX || '';
+export const ASSET_PREFIX = process.env.ASSET_PREFIX || '';
 export const KRATOS_PUBLIC_URL = process.env.NEXT_PUBLIC_KRATOS_PUBLIC_URL as string;
