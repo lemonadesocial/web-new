ca480095247a07080beb2c1e8e890185bdeeff7d
Chris Nguyen <chris.dungnguyen@gmail.com>
2025-03-29 08:00:29 +0700

    feat: update metadata for community

--- app/[domain]/(blank)/c/[uid]/page.tsx
+++ app/[domain]/(blank)/c/[uid]/page.tsx
@@ -1,9 +1,38 @@
 import { notFound } from 'next/navigation';
+import { ResolvingMetadata } from 'next';
 
 import { getClient } from '$lib/request/client';
 import { isObjectId } from '$lib/utils/helpers';
 import { GetSpaceDocument, Space } from '$lib/generated/backend/graphql';
 import { Community } from '$lib/components/features/community';
+import { generateUrl } from '$lib/utils/cnd';
+
+type Props = { params: Promise<{ domain: string }> };
+
+export async function generateMetadata({ params }: Props, parent: ResolvingMetadata) {
+  const res = await params;
+  const domain = decodeURIComponent(res.domain);
+
+  const client = getClient();
+
+  const { data } = await client.query({ query: GetSpaceDocument, variables: { hostname: domain } });
+  const space = data?.getSpace as Space;
+
+  const previousImages = (await parent).openGraph?.images || [];
+  let images = [...previousImages];
+
+  if (space?.image_cover_expanded) {
+    images = [generateUrl(space?.image_cover_expanded), ...images];
+  }
+
+  return {
+    title: space?.title,
+    description: space?.description,
+    openGraph: {
+      images,
+    },
+  };
+}
 
 export default async function Page({ params }: { params: Promise<{ uid: string }> }) {
   const uid = (await params).uid;
