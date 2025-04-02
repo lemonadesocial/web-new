a45b681f1c291a6284194063cffcd4270d3d6c68
Chris Nguyen <chris.dungnguyen@gmail.com>
2025-04-02 16:41:27 +0700

    fix: revert

--- middleware.ts
+++ middleware.ts
@@ -27,10 +27,10 @@ export default async function middleware(req: NextRequest) {
 
   const client = getClient();
   const { data } = await client.query({ query: GetSpaceDocument, variables: { hostname: hostname } });
-  // if (data.getSpace) {
-  //   return NextResponse.rewrite(new URL(`/${hostname}/community${path === '/' ? '' : path}`, req.url));
-  // }
+  if (data.getSpace) {
+    return NextResponse.rewrite(new URL(`/${hostname}/community${path === '/' ? '' : path}`, req.url));
+  }
 
   // rewrite everything else to `/[domain]/path dynamic route
-  return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
+  // return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
 }
