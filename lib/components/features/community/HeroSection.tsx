836898dc23463509e4187ac9c465def73fc17318
Chris Nguyen <chris.dungnguyen@gmail.com>
2025-04-11 09:44:26 +0700

    fix: update bg bottomsheet

--- lib/components/features/community/HeroSection.tsx
+++ lib/components/features/community/HeroSection.tsx
@@ -171,7 +171,7 @@ export function HeroSection({ space }: HeroSectionProps) {
       <Sheet ref={sheetRef} isOpen={openSheet} onClose={handleCloseSheet} snapPoints={[324]} initialSnap={0}>
         <Sheet.Backdrop onTap={handleCloseSheet} />
 
-        <Sheet.Container className="bg-overlay-backdrop! rounded-tl-lg! rounded-tr-lg! backdrop-blur-2xl">
+        <Sheet.Container className="bg-overlay-primary/80! rounded-tl-lg! rounded-tr-lg! backdrop-blur-2xl">
           <Sheet.Header className="rounded-tl-lg rounded-tr-lg">
             <div className="flex justify-center items-end h-[20px]">
               <div className="bg-primary/8 rounded-xs w-[48px] h-1 cursor-row-resize"></div>
