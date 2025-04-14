1ec735d05d30e3dc3d6a687ad6fe3add45858689
Nam Nguyen <namhoang.td09@gmail.com>
2025-04-14 08:33:49 +0700

    feat: add dice avatar

--- lib/utils/user.ts
+++ lib/utils/user.ts
@@ -1,4 +1,8 @@
+import { createAvatar } from '@dicebear/core';
+import * as thumbs from '@dicebear/thumbs';
+
 import { User } from "$lib/generated/backend/graphql";
+
 import { EDIT_KEY, generateUrl } from "./cnd";
 
 export function userAvatar(user: User, edits: keyof typeof EDIT_KEY = 'PROFILE') {
@@ -13,17 +17,27 @@ export function userAvatar(user: User, edits: keyof typeof EDIT_KEY = 'PROFILE')
   return randomUserImage(user?._id || '');
 }
 
-function randomImage(id: string, length: number, template: (num: number) => string): string {
-  const idLength = id ? id.length : 0;
-
-  let hash = 0;
-  for (let i = 0; i < idLength; i++) {
-    hash += id.charCodeAt(i);
-  }
-
-  return template((hash % length) + 1);
-}
-
 export function randomUserImage(id: string): string {
-  return randomImage(id, 10, (num) => `${process.env.ASSET_PREFIX || ''}/assets/images/avatars/lemonade_davatar_${num}.png`);
+  const avatar = createAvatar(thumbs, {
+    seed: id,
+    flip: false,
+    rotate: 0,
+    scale: 100,
+    radius: 0,
+    size: 64,
+    backgroundColor: ['b6e3f4', 'd1d4f9', 'f1f4dc', 'ffd5dc', 'ffdfbf'],
+    backgroundType: ['gradientLinear'],
+    backgroundRotation: [-90],
+    translateX: 0,
+    translateY: 0,
+    clip: false,
+    eyes: ['variant2W16', 'variant3W16', 'variant4W16', 'variant5W16', 'variant6W16', 'variant7W16', 'variant8W16', 'variant9W16'],
+    eyesColor: ['000000'],
+    face: ['variant1'],
+    mouth: ['variant1', 'variant2', 'variant3', 'variant4'],
+    mouthColor: ['000000'],
+    shapeColor: ['transparent'],
+  });
+
+  return avatar.toDataUri();
 }
