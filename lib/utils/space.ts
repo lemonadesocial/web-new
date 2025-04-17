4a343b216a558309373e3dc9c2b5595de6c71437
Nam Nguyen <namhoang.td09@gmail.com>
2025-04-17 15:21:27 +0700

    fix: nullable space

--- lib/utils/space.ts
+++ lib/utils/space.ts
@@ -9,18 +9,22 @@ export interface SpaceHydraKeys {
 }
 
 const getSpaceData = async (hostname: string): Promise<SpaceHydraKeys | null> => {
-  const spacesCollection = await collection<SpaceHydraKeys>('spaces');
-  const space = await spacesCollection.findOne(
-    { hostnames: hostname },
-    { projection: { _id: 1, hydra_client_id: 1, hydra_client_secret: 1 } }
-  );
-  
-  if (!space) return null;
-  
-  return {
-    ...space,
-    _id: space._id.toString(),
-  };
+  try {
+    const spacesCollection = await collection<SpaceHydraKeys>('spaces');
+    const space = await spacesCollection.findOne(
+      { hostnames: hostname },
+      { projection: { _id: 1, hydra_client_id: 1, hydra_client_secret: 1 } }
+    );
+    
+    if (!space) return null;
+    
+    return {
+      ...space,
+      _id: space._id.toString(),
+    };
+  } catch (error) {
+    return null;
+  }
 };
 
 export const getSpace = unstable_cache(
