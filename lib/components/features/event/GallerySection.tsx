68576831fcbf4636a6ede72b45c8d4cdf175afb4
Nam Nguyen <namhoang.td09@gmail.com>
2025-05-02 12:52:46 +0700

    fix: hide cover photo in gallery

--- lib/components/features/event/GallerySection.tsx
+++ lib/components/features/event/GallerySection.tsx
@@ -6,16 +6,19 @@ import { generateUrl } from '$lib/utils/cnd';
 
 export function GallerySection({ event, loading }: { event: Event; loading?: boolean }) {
   if (loading) return <GallerySectionSkeleton />;
-  if (!event?.new_new_photos_expanded?.length) return null;
+
+  const photos = event?.new_new_photos_expanded?.slice(1) ?? [];
+
+  if (!photos.length) return null;
 
   return (
     <div className="event-description flex flex-col gap-2 w-full">
       <p className="font-medium text-sm">
-        {event.new_new_photos_expanded?.length} Photo{event.new_new_photos_expanded?.length > 1 ? 's' : ''}
+        {photos.length} {photos.length > 1 ? 'Photos' : 'Photo'}
       </p>
       <Divider className="h-1 w-full mb-2" />
       <div className="flex md:grid md:grid-cols-3 gap-3 overflow-auto no-scrollbar">
-        {event.new_new_photos_expanded.map((photo, idx) => (
+        {photos.map((photo, idx) => (
           <img
             key={idx}
             src={generateUrl(photo)}
