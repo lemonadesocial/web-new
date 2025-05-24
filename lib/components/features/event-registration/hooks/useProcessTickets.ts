751d018cb28706db0b372773d342a4881bb464a7
Nam Nguyen <namhoang.td09@gmail.com>
2025-05-24 18:57:16 +0700

    fix: accept event non logged in

--- lib/components/features/event-registration/hooks/useProcessTickets.ts
+++ lib/components/features/event-registration/hooks/useProcessTickets.ts
@@ -1,10 +1,15 @@
+import { AcceptEventDocument } from "$lib/graphql/generated/backend/graphql";
+import { useMutation } from "$lib/graphql/request";
 import { useSession } from "$lib/hooks/useSession";
 import { TicketsProcessingModal } from "../modals/TicketsProcessingModal";
-import { nonLoggedInStatusAtom, registrationModal, useSetAtom } from "../store";
+import { buyerInfoAtom, eventDataAtom, nonLoggedInStatusAtom, registrationModal, useAtomValue, useSetAtom } from "../store";
 
 export function useProcessTickets() {
   const session = useSession();
   const setNonLoggedInStatus = useSetAtom(nonLoggedInStatusAtom);
+  const [acceptEvent] = useMutation(AcceptEventDocument);
+  const event = useAtomValue(eventDataAtom);
+  const buyerInfo = useAtomValue(buyerInfoAtom);
 
   return () => {
     if (session?.user) {
@@ -14,5 +19,11 @@ export function useProcessTickets() {
 
     registrationModal.close();
     setNonLoggedInStatus('success');
+    acceptEvent({
+      variables: {
+        id: event._id,
+        email: buyerInfo?.email,
+      },
+    });
   }
 }
