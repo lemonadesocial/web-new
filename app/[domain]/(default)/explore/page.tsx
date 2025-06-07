ac7801b30eeff6aaa97c7bc8bc2e4373092779d8
Chris Nguyen <chris.dungnguyen@gmail.com>
2025-06-07 19:20:40 +0700

    fix: missing component

--- app/[domain]/(default)/explore/page.tsx
+++ app/[domain]/(default)/explore/page.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import PageHeader from '../page-header';
 
 export default function ExplorePage() {
   return (
     <>
       <div className="flex flex-col gap-8">
-        <PageHeader
-          title="Explorer"
-          subtitle="Explore popular events near you, browse by category, or check out some of the great community hubs."
-        />
-
-        <div>asdasd</div>
+        <div>Explore</div>
       </div>
     </>
   );
