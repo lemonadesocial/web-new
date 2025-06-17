a7900f6c22ac9d7cfc530d69eeeb92a4ec5de67f
Nam Nguyen <namhoang.td09@gmail.com>
2025-06-17 13:17:05 +0700

    feat: update lens signer

--- lib/hooks/useSigner.ts
+++ lib/hooks/useSigner.ts
@@ -1,17 +1,28 @@
-import { useAppKitProvider } from "$lib/utils/appkit";
-import { Eip1193Provider, ethers } from "ethers";
 import { useEffect, useState } from "react";
+
+import { Eip1193Provider } from "ethers";
+import { BrowserProvider, Provider, Signer } from "zksync-ethers";
+import { getDefaultProvider, Network } from "@lens-chain/sdk/ethers";
+
+import { useAppKitProvider } from "$lib/utils/appkit";
 
 export function useSigner() {
   const { walletProvider } = useAppKitProvider('eip155');
-  const [signer, setSigner] = useState<ethers.JsonRpcSigner | null>(null);
+  const [signer, setSigner] = useState<Signer | null>(null);
 
   useEffect(() => {
     if (!walletProvider) return;
 
     (async () => {
-      const browserProvider = new ethers.BrowserProvider(walletProvider as Eip1193Provider);
-      const signer = await browserProvider.getSigner();
+      const browserProvider = new BrowserProvider(walletProvider as Eip1193Provider);
+      const network = await browserProvider.getNetwork();
+
+      const signer = Signer.from(
+        await browserProvider.getSigner(),
+        Number(network.chainId),
+        getDefaultProvider(process.env.NEXT_PUBLIC_APP_ENV === 'production' ? Network.Mainnet : Network.Testnet) as unknown as Provider
+      );
+      
       setSigner(signer);
     })();
   }, [walletProvider]);
