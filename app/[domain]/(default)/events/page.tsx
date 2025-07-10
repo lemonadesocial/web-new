8cb0dbaee5d7f373f12b155a2edee23dfa0337c8
Chris Nguyen <chris.dungnguyen@gmail.com>
2025-07-11 06:52:50 +0700

    feat: hide lemonheads feature

--- app/[domain]/(default)/events/page.tsx
+++ app/[domain]/(default)/events/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div className="flex flex-col-reverse md:grid md:grid-cols-[1fr_336px] gap-5 md:gap-[72px] items-start pb-10 mt-6 md:mt-11">
       <EventsContent />
-      <ClaimLemonHeadCard />
+      {/* <ClaimLemonHeadCard /> */}
     </div>
   );
 }
