a8e489968feae8d149e55ace640a33a2e51ed6ca
Chris Nguyen <chris.dungnguyen@gmail.com>
2025-07-17 17:48:00 +0700

    fix: update buttons action profile page

--- app/[domain]/(default)/profile/tabs.tsx
+++ app/[domain]/(default)/profile/tabs.tsx
@@ -1,32 +1,32 @@
-'use client'
-import { useRouter, usePathname } from 'next/navigation'
+'use client';
+import { Button } from '$lib/components/core';
+import { useRouter, usePathname } from 'next/navigation';
 
 const tabData = [
   { label: 'Feed', path: 'feed', icon: 'icon-newspaper' },
   { label: 'Events', path: 'events', icon: 'icon-ticket' },
   { label: 'Communities', path: 'communities', icon: 'icon-community' },
-]
+];
 
 export function Tabs() {
-  const router = useRouter()
-  const pathname = usePathname()
+  const router = useRouter();
+  const pathname = usePathname();
   const base = pathname.split('/profile')[0] + '/profile';
-  const active = tabData.findIndex(tab => pathname.startsWith(`${base}/${tab.path}`));
+  const active = tabData.findIndex((tab) => pathname.startsWith(`${base}/${tab.path}`));
 
   return (
-    <div className="grid grid-cols-3 gap-3">
-      {tabData.map((tab, i) => (
-        <div
-          key={tab.label}
-          className={`p-4 rounded-lg border flex justify-center cursor-pointer select-none ${active === i ? 'border-primary text-primary' : 'border-tertiary text-tertiary'}`}
-          onClick={() => router.push(`${base}/${tab.path}`)}
-          aria-selected={active === i}
-          role="tab"
-          tabIndex={active === i ? 0 : -1}
-        >
-          <i className={`${tab.icon} size-8`} />
-        </div>
-      ))}
+    <div className="w-full">
+      <div className="flex gap-3 overflow-auto no-scrollbar pb-4 pt-2 md:pt-0">
+        {tabData.map((tab, i) => (
+          <Button
+            key={i}
+            icon={tab.icon}
+            variant={active === i ? 'secondary' : 'tertiary'}
+            outlined
+            onClick={() => router.push(`${base}/${tab.path}`)}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
