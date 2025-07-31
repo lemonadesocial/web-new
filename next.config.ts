394c373565b8a6a3e129aa98ecdfa9371fbcdb32
Chris Nguyen <chris.dungnguyen@gmail.com>
2025-07-31 22:34:12 +0700

    fix: update bot

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'standalone',
   assetPrefix: '/proxy_static',
-  htmlLimitedBots: /MyBot/,
+  htmlLimitedBots:
+    /Mediapartners-Google|Chrome-Lighthouse|Slurp|DuckDuckBot|baiduspider|yandex|sogou|bitlybot|tumblr|vkShare|quora link preview|redditbot|ia_archiver|Bingbot|BingPreview|applebot|facebookexternalhit|facebookcatalog|Twitterbot|LinkedInBot|Slackbot|Discordbot|WhatsApp|SkypeUriPreview|Yeti|MyBot/,
   typescript: {
     // ignore type error on some files generated such as farcaster
     ignoreBuildErrors: true,
