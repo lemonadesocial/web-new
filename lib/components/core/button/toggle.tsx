fe2416cf567f86d2b3640e6c45d714453fd6b694
Chris Nguyen <chris.dungnguyen@gmail.com>
2025-08-11 10:40:26 +0700

    feat: missing value schedule datetime

--- lib/components/core/button/toggle.tsx
+++ lib/components/core/button/toggle.tsx
@@ -1,12 +1,15 @@
+import clsx from 'clsx';
 import React from 'react';
 
 export function Toggle({
   id,
   checked = false,
   onChange,
+  disabled = false,
 }: {
   id: string;
   checked?: boolean;
+  disabled?: boolean;
   onChange: (value: boolean) => void;
 }) {
   return (
@@ -16,10 +19,19 @@ export function Toggle({
         type="checkbox"
         className="peer sr-only"
         checked={checked}
-        onChange={(e) => onChange(e.target.checked)}
+        onChange={(e) => {
+          if (!disabled) {
+            onChange(e.target.checked);
+          }
+        }}
       />
       <label htmlFor={id} className="hidden"></label>
-      <div className="peer h-6 w-11 rounded-full border bg-quaternary after:absolute after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-success-700 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-green-300"></div>
+      <div
+        className={clsx(
+          "peer h-6 w-11 rounded-full border bg-quaternary after:absolute after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-success-700 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-green-300",
+          disabled && 'opacity-50 cursor-not-allowed',
+        )}
+      ></div>
     </label>
   );
 }
