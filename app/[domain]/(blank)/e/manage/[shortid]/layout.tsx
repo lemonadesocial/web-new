3ebd2f0cfab02bc03fa7f06f7a28cc94bd04a50e
Nam Nguyen <namhoang.td09@gmail.com>
2025-08-12 16:21:34 +0700

    feat: update cohost with email

--- app/[domain]/(blank)/e/manage/[shortid]/layout.tsx
+++ app/[domain]/(blank)/e/manage/[shortid]/layout.tsx
@@ -1,20 +1,3 @@
-import { notFound, redirect } from 'next/navigation';
-
-import { Event, GetEventDocument } from '$lib/graphql/generated/backend/graphql';
-import { getClient } from '$lib/graphql/request';
 import { EventManageLayout } from '$lib/components/features/event-manage/EventManageLayout';
 
-type LayoutProps = React.PropsWithChildren & {
-  params: { shortid: string };
-};
-
-export default async function Layout({ children, params }: LayoutProps) {
-  const shortid = (await params).shortid;
-
-  const client = getClient();
-  const { data } = await client.query({ query: GetEventDocument, variables: { shortid } });
-
-  if (!data?.getEvent) return notFound();
-
-  return <EventManageLayout event={data.getEvent as Event}>{children}</EventManageLayout>;
-}
+export default EventManageLayout;
