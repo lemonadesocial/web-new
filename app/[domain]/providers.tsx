0a58f9f89be77d2fa202a792f9063ea6831c477f
Hoang Nam <namhoang.td09@gmail.com>
2025-08-14 15:45:10 +0700

    Revert "Revert "feat(login): login with Unicorn wallet""

--- app/[domain]/providers.tsx
+++ app/[domain]/providers.tsx
@@ -4,15 +4,22 @@ import { useSetAtom } from 'jotai';
 
 import { GraphqlClientProvider } from '$lib/graphql/request';
 import { initializeAppKit } from '$lib/utils/appkit';
-import { useAuth } from '$lib/hooks/useAuth';
 import { useListChains } from '$lib/hooks/useListChains';
 import { SpaceHydraKeys } from '$lib/utils/space';
 import { hydraClientIdAtom } from '$lib/jotai';
 import { defaultClient } from '$lib/graphql/request/instances';
-import { useResumeSession as useLensResumeSession} from '$lib/hooks/useLens';
+import { useResumeSession as useLensResumeSession } from '$lib/hooks/useLens';
+import { useAuth } from "../../lib/hooks/useAuth";
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const { loading } = useAuth();
+
+  if (loading) return null;
+
+  return children;
+}
 
 export default function Providers({ children, space }: { children: React.ReactNode; space?: SpaceHydraKeys | null; }) {
-  const oryLoading = useAuth(space?.hydra_client_id);
   const chainsLoading = useListChains();
   const setHydraClientId = useSetAtom(hydraClientIdAtom);
   const [appKitReady, setAppKitReady] = React.useState(false);
@@ -31,11 +38,13 @@ export default function Providers({ children, space }: { children: React.ReactNo
     }
   }, [space]);
 
-  if (oryLoading || chainsLoading || !appKitReady) return null;
+  if (chainsLoading || !appKitReady) return null;
 
   return (
     <GraphqlClientProvider client={defaultClient}>
-      {children}
+      <AuthProvider>
+        {children}
+      </AuthProvider>
     </GraphqlClientProvider>
   );
 }
--- lib/hooks/useLogout.ts
+++ lib/hooks/useLogout.ts
@@ -4,7 +4,7 @@ import { ory } from '$lib/utils/ory';
 import { hydraClientIdAtom, sessionAtom } from '$lib/jotai';
 import { useOAuth2 } from './useOAuth2';
 
-export function useLogOut() {
+export function useRawLogout() {
   const setSession = useSetAtom(sessionAtom);
   const hydraClientId = useAtomValue(hydraClientIdAtom);
   const { signOut } = useOAuth2();
@@ -17,7 +17,19 @@ export function useLogOut() {
 
     setSession(null);
     localStorage.clear();
-    window.location.reload();
+  };
+}
+
+export function useLogOut() {
+  const rawLogout = useRawLogout();
+
+  return async () => {
+    await rawLogout();
+
+    //-- remove the auth cookie from the url and reload
+    const url = new URL(window.location.href);
+    url.searchParams.delete('authCookie');
+    window.location.href = url.toString();
   };
 };
 
