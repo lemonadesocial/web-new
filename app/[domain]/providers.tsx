2dccfbcc7e7262f4855d88bbbe14ae95a7edb39b
Thong Dang <thong.dang@coderpush.com>
2025-08-15 15:08:11 +0700

    feat: set mini app ready

--- app/[domain]/providers.tsx
+++ app/[domain]/providers.tsx
@@ -1,6 +1,7 @@
 'use client';
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { useSetAtom } from 'jotai';
+import { sdk } from '@farcaster/miniapp-sdk';
 
 import { GraphqlClientProvider } from '$lib/graphql/request';
 import { initializeAppKit } from '$lib/utils/appkit';
@@ -9,33 +10,46 @@ import { SpaceHydraKeys } from '$lib/utils/space';
 import { hydraClientIdAtom } from '$lib/jotai';
 import { defaultClient } from '$lib/graphql/request/instances';
 import { useResumeSession as useLensResumeSession } from '$lib/hooks/useLens';
-import { useAuth } from "../../lib/hooks/useAuth";
+import { useAuth } from '../../lib/hooks/useAuth';
 
-export default function Providers({ children, space }: { children: React.ReactNode; space?: SpaceHydraKeys | null; }) {
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const { loading } = useAuth(true);
+
+  if (loading) return null;
+
+  return children;
+}
+
+export default function Providers({ children, space }: { children: React.ReactNode; space?: SpaceHydraKeys | null }) {
+  const [miniAppReady, setMiniAppReady] = useState(false);
   const chainsLoading = useListChains();
   const setHydraClientId = useSetAtom(hydraClientIdAtom);
-  const [appKitReady, setAppKitReady] = React.useState(false);
+  const [appKitReady, setAppKitReady] = useState(false);
   useLensResumeSession();
   const { loading: loadingAuth } = useAuth(true);
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (!chainsLoading) {
       initializeAppKit();
       setAppKitReady(true);
     }
   }, [chainsLoading]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (space?.hydra_client_id) {
       setHydraClientId(space.hydra_client_id);
     }
   }, [space]);
 
-  if (chainsLoading || !appKitReady || loadingAuth) return null;
+  useEffect(() => {
+    sdk.actions.ready().then(() => setMiniAppReady(true));
+  }, []);
+
+  if (chainsLoading || !appKitReady || !miniAppReady || loadingAuth) return null;
 
   return (
     <GraphqlClientProvider client={defaultClient}>
-      {children}
+      <AuthProvider>{children}</AuthProvider>
     </GraphqlClientProvider>
   );
 }
