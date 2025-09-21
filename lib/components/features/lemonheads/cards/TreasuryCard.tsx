93af9eeff2792b1a3e151add6fbd184a89b3f981
Chris Nguyen <chris.dungnguyen@gmail.com>
2025-09-21 14:27:25 +0700

    feat: add event to lemonheads community

--- lib/components/features/lemonheads/cards/TreasuryCard.tsx
+++ lib/components/features/lemonheads/cards/TreasuryCard.tsx
@@ -1,7 +1,7 @@
 export function LemonHeadsTreasuryCard() {
   return (
     <>
-      <div className="flex w-full min-w-fit items-center md:hidden p-2.5 border-(length:--card-border-width) rounded-md gap-2.5">
+      <div className="flex w-full min-w-fit items-center md:hidden p-2.5 border-(length:--card-border-width) border-card-border rounded-md gap-2.5">
         <div className="flex justify-center items-center rounded-sm bg-success-500/16 size-8 p-1.5 aspect-square">
           <i className="icon-account-balance-outline text-success-500" />
         </div>
@@ -12,7 +12,7 @@ export function LemonHeadsTreasuryCard() {
         </div>
       </div>
 
-      <div className="hidden md:flex p-4 border-(length:--card-border-width) rounded-md flex-col gap-3">
+      <div className="hidden md:flex p-4 border-(length:--card-border-width) border-card-border  rounded-md flex-col gap-3">
         <div className="flex justify-between">
           <div className="flex justify-center items-center rounded-full bg-success-500/16 size-[48px] aspect-square">
             <i className="icon-account-balance-outline text-success-500" />
