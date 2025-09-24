5a82a65cef4b957df64f8d321238e00750773270
Nam Nguyen <nam.nguyen@hideandseek.digital>
2025-09-24 19:40:11 +0700

    feat!: council members

--- app/[domain]/(blank)/s/lemonheads/treasury/page.tsx
+++ app/[domain]/(blank)/s/lemonheads/treasury/page.tsx
@@ -1,5 +1,7 @@
+import { Spacer } from '$lib/components/core';
+import { CouncilMembers } from '$lib/components/features/lemonheads/CouncilMembers';
 import { LemonHeadsTreasury } from '$lib/components/features/lemonheads/LemonHeadsTreasury';
-import { SubTitleSection, TitleSection } from '../shared';
+import { JourneySection, SubTitleSection, TitleSection } from '../shared';
 
 function Page() {
   return (
@@ -9,6 +11,16 @@ function Page() {
         <SubTitleSection>Shared vault for the community. Create & vote on proposals to access funds.</SubTitleSection>
       </div>
 
+      <JourneySection />
+      <div className="h-9" />
+
+      <div className="flex flex-col gap-6">
+        <hr className="border border-divider" />
+        <CouncilMembers />
+        <hr className="border border-divider" />
+      </div>
+
+      <div className="h-6" />
       <LemonHeadsTreasury />
     </div>
   );
