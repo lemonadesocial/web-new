7a19d43dddcf743eb6a98f883ab500fee0d8216d
Chris Nguyen <chris.dungnguyen@gmail.com>
2025-09-25 13:38:43 +0700

    feat: update feed post detail

--- app/[domain]/(blank)/s/lemonheads/posts/[slug]/page.tsx
+++ app/[domain]/(blank)/s/lemonheads/posts/[slug]/page.tsx
@@ -7,7 +7,7 @@ export default async function Page({ params }: { params: Promise<{ slug: string 
   if (!slug) return notFound();
 
   return (
-    <div className="mt-6">
+    <div className="md:mt-6">
       <FeedPostDetail postSlug={slug} />
     </div>
   );
