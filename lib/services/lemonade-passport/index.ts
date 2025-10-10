0d1b9716341f6e8f96c63e77a911336f4cce9a3f
Thong Dang <thong.dang@coderpush.com>
2025-10-10 07:24:46 +0700

    feat: implement lemonade-passport render

---
+++ lib/services/lemonade-passport/index.ts
@@ -0,0 +1,144 @@
+import { getData } from '$lib/services/lemonhead/admin';
+import { getImageFromBuffers } from '$lib/services/nft/image';
+import { getUriFromUrl, uploadImage, uploadJSON } from '$lib/services/nft/storage';
+import assert from 'assert';
+import fs from 'fs';
+import moment from 'moment';
+import path from 'path';
+import { Canvas, Image } from 'canvas';
+
+import { getApproval } from "./admin";
+
+const DESCRIPTION = 'Lemonade Passport is the enttry point to Lemonade Ecosystem';
+
+const outputWidth = 2160;
+const outputHeight = 1350;
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+const avatarOffset: Point = {
+  x: 175,
+  y: 367,
+};
+
+const avatarSize = 610;
+const fontSize = 60;
+
+const usernameOffset: Point = {
+  x: 100,
+  y: 100,
+};
+
+const passportIdOffset: Point = {
+  x: 960,
+  y: 720,
+};
+
+const creationDateOffset: Point = {
+  x: 100,
+  y: 100,
+};
+
+const createMetadata = (imageUrl: string) => {
+  return {
+    description: DESCRIPTION,
+    image: imageUrl,
+  };
+};
+
+const getAvatarImageBuffer = async (avatarImageUrl: string) => {
+  //-- create a canvas of output size
+  const canvas = new Canvas(outputWidth, outputHeight);
+  const ctx = canvas.getContext('2d');
+  assert.ok(ctx);
+  //-- draw the avatar image
+  const img = new Image();
+
+  await new Promise<void>((resolve, reject) => {
+    img.onload = () => {
+      // Scale the image to fit the canvas dimensions
+      ctx.drawImage(img, avatarOffset.x, avatarOffset.y, avatarSize, avatarSize);
+      resolve();
+    };
+    img.onerror = (err) => {
+      reject(err);
+    };
+    img.src = avatarImageUrl;
+  });
+
+  return canvas.toBuffer('image/png');
+}
+
+const getBoilerplateImageBuffer = async () => {
+  const filePath = path.join(process.cwd(), "data", "passport", "lemonade-passport.png");
+  return fs.readFileSync(filePath);
+}
+
+const getTextImageBuffer = async (text: string, offset: Point) => {
+
+}
+
+const getUsernameImageBuffer = async (username: string) => {
+  return getTextImageBuffer(username, usernameOffset);
+}
+
+const getPassportIdImageBuffer = async (passportId: string) => {
+  return getTextImageBuffer(passportId, passportIdOffset);
+}
+
+const getCreationDateImageBuffer = async (creationDate: string) => {
+  return getTextImageBuffer(creationDate, creationDateOffset);
+}
+
+export const getMintLemonadePassportData = async (wallet: string, username: string, avatarNftContractAddress: string) => {
+  const lemonheadData = await getData(wallet);
+
+  assert.ok(lemonheadData && lemonheadData.tokenId && lemonheadData.imageUrl);
+
+  const passportId = lemonheadData.tokenId.padStart(8, '0');
+
+  const creationDate = moment().format('MM/DD/YYYY');
+
+  const buffers = await Promise.all([
+    getAvatarImageBuffer(lemonheadData.imageUrl),
+    getBoilerplateImageBuffer(),
+    // getUsernameImageBuffer(username),
+    // getPassportIdImageBuffer(passportId),
+    // getCreationDateImageBuffer(creationDate),
+  ]);
+
+  const finalImage = await getImageFromBuffers(
+    buffers,
+    outputWidth,
+    outputHeight,
+    'png',
+  );
+
+  const fileId = `lemonade-passport-${passportId}`;
+  const imageUrl = await uploadImage(`${fileId}.png`, finalImage);
+
+  //-- create and upload metadata
+  const metadata = createMetadata(imageUrl);
+  const metadataUrl = await uploadJSON(fileId, { ...metadata });
+  const uri = getUriFromUrl(metadataUrl);
+
+  //-- call backend API and obtain the signature
+  const data = await getApproval(wallet, uri);
+
+  if (!data) {
+    throw new Error('Failed to get minting approval');
+  }
+
+  return {
+    //-- use these to call the contract minting function
+    signature: data.signature,
+    price: data.price,
+    metadata: getUriFromUrl(metadataUrl),
+
+    //-- for display purposes
+    image: imageUrl,
+  };
+};
