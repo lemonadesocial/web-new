03534b692062eebfcbf031408f7e291e0a43b854
Chris Nguyen <chris.dungnguyen@gmail.com>
2025-10-11 14:47:22 +0700

    feat: update layout mweb photo

--- lib/components/features/passport/mint/steps/PhotoCards.tsx
+++ lib/components/features/passport/mint/steps/PhotoCards.tsx
@@ -17,7 +17,9 @@ export function LemonheadCard() {
 
   const isSelected = state.useLemonhead;
   return (
-    <div className={`flex flex-col gap-4 rounded-md bg-card p-4 transition-all border ${isSelected ? 'border-primary' : 'border-card-border'}`}>
+    <div
+      className={`flex flex-col gap-4 rounded-md bg-card p-4 transition-all border ${isSelected ? 'border-primary' : 'border-card-border'}`}
+    >
       {loading || !data ? (
         <>
           <Skeleton animate className="w-full h-64 rounded-sm" />
@@ -28,13 +30,13 @@ export function LemonheadCard() {
         </>
       ) : (
         <>
-          <img src={data.image} className={clsx("w-full h-full border-card-border rounded-sm")} />
+          <img src={data.image} className={clsx('w-full aspect-square border-card-border rounded-sm')} />
           <div className="flex flex-col gap-1">
             <p>LemonHead #{data.tokenId}</p>
-            <p className="text-sm text-tertiary">Set LemonHead as Passport Photo.</p>
+            <p className="hidden md:block text-sm text-tertiary">Set LemonHead as Passport Photo.</p>
           </div>
           <Button
-            variant={isSelected ? "tertiary" : "secondary"}
+            variant={isSelected ? 'tertiary' : 'secondary'}
             className="w-full"
             onClick={handleSelect}
             iconLeft={isSelected ? 'icon-done' : undefined}
@@ -72,14 +74,16 @@ export function FluffleCard() {
 
   if (data?.hasToken) {
     return (
-      <div className={`flex flex-col gap-4 rounded-md bg-card p-4 transition-all border ${isSelected ? 'border-primary' : 'border-card-border'}`}>
+      <div
+        className={`flex flex-col gap-4 rounded-md bg-card p-4 transition-all border ${isSelected ? 'border-primary' : 'border-card-border'}`}
+      >
         <img src={data.image} className="w-full h-64 object-cover border-card-border rounded-sm" />
         <div className="flex flex-col gap-1">
           <p>Fluffle #{data.tokenId}</p>
-          <p className="text-sm text-tertiary">Set Fluffle as Passport Photo.</p>
+          <p className="hidden md:block text-sm text-tertiary">Set Fluffle as Passport Photo.</p>
         </div>
         <Button
-          variant={isSelected ? "tertiary" : "secondary"}
+          variant={isSelected ? 'tertiary' : 'secondary'}
           className="w-full"
           onClick={handleSelect}
           iconLeft={isSelected ? 'icon-done' : undefined}
@@ -93,21 +97,20 @@ export function FluffleCard() {
 
   return (
     <div className="flex flex-col gap-4 rounded-md bg-card p-4 transition-all border-2 border-dashed border-card-border">
-      <img src={`${ASSET_PREFIX}/assets/images/fluffle.png`} className="w-full" />
+      <img src={`${ASSET_PREFIX}/assets/images/fluffle.png`} className="w-full aspect-square" />
       <div className="flex flex-col gap-1">
-          <p>Get Fluffle</p>
-          <p className="text-sm text-tertiary">You don’t have a fluffle, yet.</p>
-        </div>
-        <Button
-          variant={isSelected ? "tertiary" : "secondary"}
-          className="w-full"
-          onClick={handleSelect}
-          iconLeft={isSelected ? 'icon-done' : undefined}
-          size="sm"
-        >
-          {isSelected ? 'Selected' : 'Select'}
-        </Button>
+        <p>Get Fluffle</p>
+        <p className="hidden md:block text-sm text-tertiary">You don’t have a fluffle, yet.</p>
+      </div>
+      <Button
+        variant={isSelected ? 'tertiary' : 'secondary'}
+        className="w-full"
+        onClick={handleSelect}
+        iconLeft={isSelected ? 'icon-done' : undefined}
+        size="sm"
+      >
+        {isSelected ? 'Selected' : 'Select'}
+      </Button>
     </div>
   );
 }
-
