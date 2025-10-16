35f3bbb67a6f7226c74bb63f1a0bd367a9ad01b4
Chris Nguyen <chris.dungnguyen@gmail.com>
2025-10-16 10:35:21 +0700

    feat: update filter people

--- app/[domain]/(blank)/s/manage/[uid]/page.tsx
+++ app/[domain]/(blank)/s/manage/[uid]/page.tsx
@@ -3,12 +3,16 @@ import { CommunityEvents } from '$lib/components/features/community-manage/Commu
 import { getSpace } from '$lib/utils/getSpace';
 import { isObjectId } from '$lib/utils/helpers';
 import { CommunityOverview } from '$lib/components/features/community-manage/CommunityOverview';
+import { getClient } from '$lib/graphql/request';
+import { GetSpaceDocument, Space } from '$lib/graphql/generated/backend/graphql';
 
 export default async function Page({ params }: { params: Promise<{ uid: string }> }) {
   const uid = (await params).uid;
   const variables = isObjectId(uid) ? { id: uid, slug: uid } : { slug: uid };
 
-  const space = await getSpace(variables);
+  const client = getClient();
+  const { data } = await client.query({ query: GetSpaceDocument, variables });
+  const space = data?.getSpace as Space;
 
   if (!space) return notFound();
 
--- lib/components/features/community-manage/CommunityPeople.tsx
+++ lib/components/features/community-manage/CommunityPeople.tsx
@@ -1,30 +1,93 @@
 'use client';
 import React from 'react';
-import { Avatar, Button, InputField, Menu, Skeleton } from '$lib/components/core';
+import { Avatar, Button, Divider, InputField, Menu, MenuItem, Skeleton } from '$lib/components/core';
 import { CardTable } from '$lib/components/core/table';
-import { GetSpaceMembersDocument, Space, User } from '$lib/graphql/generated/backend/graphql';
+import {
+  GetSpaceMembersDocument,
+  GetSpaceTagsDocument,
+  Space,
+  SpaceTag,
+  SpaceTagType,
+  User,
+} from '$lib/graphql/generated/backend/graphql';
 import { useQuery } from '$lib/graphql/request';
 import { userAvatar } from '$lib/utils/user';
-import { debounce } from 'lodash';
-import { format, formatDistance, formatDuration, isBefore, isToday, subDays } from 'date-fns';
+import { debounce, snakeCase } from 'lodash';
+import { format, formatDistance, isToday } from 'date-fns';
+import clsx from 'clsx';
+import { downloadCSVFile } from '$lib/utils/file';
 
 interface Props {
   space: Space;
 }
 
 const LIMIT = 15;
-const controller = new AbortController();
+
+const SORT_BY_ITEMS: Record<string, { label: string }> = {
+  user_name: { label: 'Name' },
+  role_changed_at: { label: 'Recently Joined' },
+  //revenue: { label: 'Revenue' },
+  //events_registered: { label: 'Events Registered' },
+  //events_checked_in: { label: 'Events Checked In' },
+};
+
+const FILTER_BY_LAST_ITEMS: Record<string, { label: string; icon: string }> = {
+  unsubscribed: { label: 'Unsubscribed', icon: 'icon-person-cancel' },
+  deletion: { label: 'Removed', icon: 'icon-person-remove' },
+  //blocked: { label: 'Blocked' },
+};
+
+type FilterTagType = {
+  [key: string]: {
+    label: string;
+    count?: number;
+    icon: {
+      name: string;
+      color: string;
+    };
+  };
+};
+
 export function CommunityPeople({ space }: Props) {
   const [skip, setSkip] = React.useState(0);
   const [query, setQuery] = React.useState('');
   const [search, setSearch] = React.useState('');
+  const [sortBy, setSortBy] = React.useState('role_changed_at');
+  const [filterBy, setFilterBy] = React.useState('all');
+  const [filterByTags, setFilterByTags] = React.useState<string[]>([]);
 
   const { data, loading } = useQuery(GetSpaceMembersDocument, {
-    variables: { space: space._id, limit: LIMIT, skip, search },
+    variables: {
+      space: space._id,
+      limit: LIMIT,
+      skip,
+      search,
+      tags: filterByTags,
+      sort: {
+        field: sortBy,
+        order: -1,
+      },
+    },
     skip: !space._id,
   });
 
   const total = data?.listSpaceMembers?.total || 0;
+
+  const { data: dataGetSpaceTags } = useQuery(GetSpaceTagsDocument, {
+    variables: { space: space._id },
+    skip: !space._id,
+  });
+  const listSpaceTags = (dataGetSpaceTags?.listSpaceTags || []) as SpaceTag[];
+  const memberTags = listSpaceTags.filter((t) => t.type === SpaceTagType.Member) || [];
+  const tags = memberTags.reduce((acc: FilterTagType, { _id, tag, targets, color }) => {
+    acc[_id] = { label: tag, count: targets?.length, icon: { name: 'icon-dot', color } };
+    return acc;
+  }, {});
+
+  const handleExport = async () => {
+    const url = `/space/${space._id}/export/members?role=subscriber`;
+    await downloadCSVFile(url, snakeCase(`${space.title}_people`));
+  };
 
   React.useEffect(() => {
     const handler = debounce(() => {
@@ -47,8 +110,21 @@ export function CommunityPeople({ space }: Props) {
         <Button variant="tertiary-alt" size="sm" iconLeft="icon-user-plus">
           Add People
         </Button>
-        <Button variant="tertiary-alt" size="sm" iconLeft="icon-download" />
-        <Button variant="tertiary-alt" size="sm" iconLeft="icon-more-vert" />
+        <Button variant="tertiary-alt" size="sm" iconLeft="icon-download" onClick={handleExport} />
+        <Menu.Root>
+          <Menu.Trigger>
+            <Button variant="tertiary-alt" size="sm" iconLeft="icon-more-vert" />
+          </Menu.Trigger>
+          <Menu.Content className="p-1">
+            {({ toggle }) => (
+              <>
+                {Object.entries(FILTER_BY_LAST_ITEMS).map(([key, value]) => (
+                  <MenuItem key={key} title={value.label} iconLeft={value.icon} />
+                ))}
+              </>
+            )}
+          </Menu.Content>
+        </Menu.Root>
       </div>
 
       <InputField
@@ -61,22 +137,79 @@ export function CommunityPeople({ space }: Props) {
       />
 
       <div className="flex items-center justify-between">
-        <Menu.Root>
+        <Menu.Root placement="bottom-start">
           <Menu.Trigger>
             <Button iconLeft="icon-filter-line" iconRight="icon-chevron-down" size="sm" variant="tertiary-alt">
               Filter
             </Button>
           </Menu.Trigger>
-          <Menu.Content>content</Menu.Content>
+          <Menu.Content className="p-0 h-64 w-48 overflow-y-auto">
+            {({ toggle }) => (
+              <>
+                <div className="p-1">
+                  <MenuItem
+                    title="all"
+                    iconLeft="icon-ticket"
+                    iconRight={filterBy === 'all' ? 'icon-done' : ''}
+                    onClick={() => {
+                      setFilterBy('all');
+                      setFilterByTags([]);
+                      toggle();
+                    }}
+                  />
+                </div>
+                <Divider />
+                <div className="p-1">
+                  <p className="text-xs text-tertiary px-2">Tags</p>
+                  {Object.entries(tags).map(([key, value]) => (
+                    <MenuItem
+                      key={key}
+                      iconLeft={
+                        <i className={`text-tertiary size-4 ${value.icon.name}`} style={{ color: value.icon.color }} />
+                      }
+                      iconRight={filterByTags.includes(key) ? 'icon-done' : ''}
+                      onClick={() => {
+                        const updatedFilterByTags = filterByTags.includes(key)
+                          ? filterByTags.filter((tag) => tag !== key)
+                          : [...filterByTags, key];
+                        setFilterByTags(updatedFilterByTags);
+                        setFilterBy('');
+                      }}
+                    >
+                      <p className="font-medium text-sm font-default-body text-secondary flex-1 line-clamp-2 max-w-xs text-wrap">
+                        {value.label}
+                      </p>
+                    </MenuItem>
+                  ))}
+                </div>
+              </>
+            )}
+          </Menu.Content>
         </Menu.Root>
 
         <Menu.Root>
           <Menu.Trigger>
             <Button iconLeft="icon-sort" iconRight="icon-chevron-down" size="sm" variant="tertiary-alt">
-              Recently Joined
+              {SORT_BY_ITEMS[sortBy].label}
             </Button>
           </Menu.Trigger>
-          <Menu.Content>content</Menu.Content>
+          <Menu.Content className="p-1">
+            {({ toggle }) => (
+              <>
+                {Object.entries(SORT_BY_ITEMS).map(([key, value]) => (
+                  <MenuItem
+                    key={key}
+                    title={value.label}
+                    iconLeft={clsx('icon-done', key === sortBy ? 'opacity-100' : 'opacity-0')}
+                    onClick={() => {
+                      setSortBy(key);
+                      toggle();
+                    }}
+                  />
+                ))}
+              </>
+            )}
+          </Menu.Content>
         </Menu.Root>
       </div>
 
@@ -99,7 +232,7 @@ export function CommunityPeople({ space }: Props) {
             <div className="flex px-4 py-3 items-center">
               <div className="flex-1 flex gap-3 items-center">
                 <Avatar src={userAvatar(item.user_expanded as unknown as User)} />
-                <div className="flex flex-col md:flex-row md:gap-3">
+                <div className="flex flex-col md:flex-row md:gap-3 items-center">
                   <p>{item.user_name || item.user_expanded?.display_name || item.user_expanded?.name || 'Anonymous'}</p>
                   <p className="text-tertiary line-clamp-1">{item.email || item.user_expanded?.email}</p>
                 </div>
