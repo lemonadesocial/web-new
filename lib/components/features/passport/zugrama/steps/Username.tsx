40d51a58691c747e1418cf47bd457fb3e5168bb0
Nam Nguyen <namhoang.td09@gmail.com>
2025-10-31 21:06:47 +0700

    fix: ens name is null

--- lib/components/features/passport/zugrama/steps/Username.tsx
+++ lib/components/features/passport/zugrama/steps/Username.tsx
@@ -22,7 +22,9 @@ export function PassportUsername() {
       try {
         const provider = new ethers.JsonRpcProvider(mainnet.rpcUrls.default.http[0]);
         const name = await provider.lookupAddress(address);
-        dispatch({ type: PassportActionKind.SetEnsName, payload: `@${name}` });
+        if (name) {
+          dispatch({ type: PassportActionKind.SetEnsName, payload: `@${name}` });
+        }
       } catch (error) {
         toast.error(formatError(error))
       } finally {
