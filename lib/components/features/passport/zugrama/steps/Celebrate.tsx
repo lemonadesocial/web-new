da13eef207c24c7d4b8cec4d372263a28259dc72
Nam Nguyen <namhoang.td09@gmail.com>
2025-11-05 15:42:02 +0700

    fix: remove video bg in zugrama

--- lib/components/features/passport/zugrama/steps/Celebrate.tsx
+++ lib/components/features/passport/zugrama/steps/Celebrate.tsx
@@ -1,41 +1,13 @@
 'use client';
-import React from 'react';
-import Player from 'video.js/dist/types/player';
-import { isMobile } from 'react-device-detect';
-import videojs from 'video.js';
-import { twMerge } from 'tailwind-merge';
 
-import { Card, Button, drawer, modal, Divider } from '$lib/components/core';
+import { Card, Button, drawer } from '$lib/components/core';
 import { usePassportContext } from '../provider';
 import { ETHERSCAN } from '$lib/utils/constants';
 import { SharedPassportPane } from '$lib/components/features/passport/SharedPassportPane';
 
 export function PassportCelebrate() {
-  const videoRef = React.useRef(null);
-  const playerRef = React.useRef<Player>(null);
-
-  const [mute, setMute] = React.useState(true);
   const [state] = usePassportContext();
   const tokenId = state.mintState?.tokenId;
-
-  React.useEffect(() => {
-    if (videoRef.current && !playerRef.current) {
-      const srcPath = isMobile ? 'lemonhead_mint_mobile' : 'lemonhead_mint_web';
-      playerRef.current = videojs(videoRef.current, {
-        fill: true,
-        loop: true,
-        controls: false,
-        autoplay: true,
-        muted: mute,
-        sources: [
-          {
-            src: `${process.env.NEXT_PUBLIC_LMD_VIDEO}/${srcPath}/video.m3u8`,
-            type: 'application/x-mpegURL',
-          },
-        ],
-      });
-    }
-  }, [videoRef.current, isMobile]);
 
   const handleShare = () => drawer.open(SharedPassportPane, { props: { tokenId: tokenId! } });
 
@@ -49,21 +21,14 @@ export function PassportCelebrate() {
             </Card.Content>
           </Card.Root>
 
-          <div className="flex flex-wrap gap-5 w-full justify-center md:justify-between">
-            <div className="flex items-center gap-2">
-              <Button
-                variant="tertiary-alt"
-                icon={mute ? 'icon-speaker-wave' : 'icon-speaker-x-mark'}
-                onClick={() => setMute(!mute)}
-              />
-              <Button
-                iconRight="icon-arrow-outward"
-                variant="tertiary-alt"
-                onClick={() => window.open(`${ETHERSCAN}/tx/${state.mintState?.txHash}`)}
-              >
-                View txn.
-              </Button>
-            </div>
+          <div className="flex flex-wrap gap-5 w-full justify-center">
+            <Button
+              iconRight="icon-arrow-outward"
+              variant="tertiary-alt"
+              onClick={() => window.open(`${ETHERSCAN}/tx/${state.mintState?.txHash}`)}
+            >
+              View txn.
+            </Button>
 
             {/* <div className="flex gap-2">
               <Button iconLeft="icon-share" variant="secondary" onClick={handleShare}>
@@ -72,18 +37,6 @@ export function PassportCelebrate() {
             </div> */}
           </div>
         </div>
-      </div>
-
-      <div className="fixed inset-0 z-0">
-        <div className="fixed inset-0 bg-background/56 z-0" />
-        <video
-          autoPlay
-          loop
-          ref={videoRef}
-          muted={mute}
-          playsInline
-          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-        ></video>
       </div>
     </div>
   );
