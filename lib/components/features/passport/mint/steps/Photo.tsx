05b366264bdf3c34f8d587fd103676473816d017
Chris Nguyen <chris.dungnguyen@gmail.com>
2025-11-05 08:25:49 +0700

    feat: update title - desc mint flow

--- lib/components/features/passport/mint/steps/Photo.tsx
+++ lib/components/features/passport/mint/steps/Photo.tsx
@@ -4,10 +4,8 @@ export function PassportPhoto() {
   return (
     <div className="flex-1 flex flex-col gap-8 md:py-12">
       <div className="space-y-2">
-        <h1 className="text-3xl font-semibold leading-tight">Show the world who you are.</h1>
-        <p className="text-tertiary">
-          Select your LemonHead or upload a custom photo to make your passport uniquely yours.
-        </p>
+        <h1 className="text-3xl font-semibold leading-tight">Choose your passport photo.</h1>
+        <p className="text-tertiary">Select the avatar you’d like on your passport.</p>
       </div>
 
       <div className="grid grid-cols-2 gap-4">
--- lib/components/features/passport/mint/steps/Username.tsx
+++ lib/components/features/passport/mint/steps/Username.tsx
@@ -6,8 +6,8 @@ export function PassportUsername() {
   return (
     <div className="flex-1 flex flex-col gap-6 md:py-12">
       <div className="flex flex-col gap-4">
-        <h1 className="text-5xl font-semibold leading-tight">Your name, your identity.</h1>
-        <p className="text-tertiary">Pick a unique username to represent you across the United Stands of Lemonade.</p>
+        <h1 className="text-5xl font-semibold leading-tight">Select passport name.</h1>
+        <p className="text-tertiary">Choose what appears on your passport, your username or ENS.</p>
       </div>
 
       <div className="grid grid-cols-2 gap-4">
