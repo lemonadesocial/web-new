08dee4cec543f6907e114c91dbf2f985c8f982f2
Nam Nguyen <namhoang.td09@gmail.com>
2025-11-10 15:29:32 +0700

    feat: update group search

--- lib/services/token-launch-pad/index.ts
+++ lib/services/token-launch-pad/index.ts
@@ -1,10 +1,6 @@
 import * as ethers from 'ethers';
 
 import ZapContractABI from '../../abis/token-launch-pad/FlaunchZap.json';
-import TreasuryManagerFactoryABI from '../../abis/token-launch-pad/TreasuryManagerFactory.json';
-import PositionManagerABI from '../../abis/token-launch-pad/PositionManager.json';
-import IERC721ABI from '../../abis/token-launch-pad/IERC721.json';
-import FlaunchABI from '../../abis/token-launch-pad/Flaunch.json';
 
 export const SECONDS_PER_DAY = 86400;
 export const DAYS_PER_MONTH = 30;
@@ -34,46 +30,6 @@ export interface CreateGroupParams {
   creatorSharePercentage: number;
   ownerSharePercentage: number;
   isOpen?: boolean;
-}
-
-export const createGroup = async (
-  zapContractAddress: string, //-- get from chain
-  stakingManagerImplementationContractAddress: string, //-- get from chain
-  closedPermissionsContractAddress: string,  //-- get from chain
-  signer: ethers.Signer, //-- signer for write operations
-  params: CreateGroupParams,
-) => {
-  const permissionsContractAddress = params.isOpen ? ethers.ZeroAddress : closedPermissionsContractAddress;
-
-  if (!permissionsContractAddress) {
-    throw new Error('Permissions contract address is required');
-  }
-
-  //-- write operation: deploy and initialize manager
-  const writeContract = new ethers.Contract(zapContractAddress, ZapContractABI.abi, signer);
-
-  const data = ethers.AbiCoder.defaultAbiCoder().encode(['address', 'uint256', 'uint256', 'uint256', 'uint256'], [
-    params.groupERC20Token,
-    params.minEscrowDuration,
-    params.minStakeDuration,
-    params.creatorSharePercentage * 100000,
-    params.ownerSharePercentage * 100000,
-  ]);
-
-  const tx: ethers.TransactionResponse = await writeContract.deployAndInitializeManager(
-    stakingManagerImplementationContractAddress, params.groupOwner, data, permissionsContractAddress
-  );
-
-  const receipt = await tx.wait();
-
-  //-- read operation: parse logs to get manager address
-  const contractInterface = new ethers.Interface(TreasuryManagerFactoryABI.abi);
-  const event = parseLogs(receipt, contractInterface).find((log) => log.parsedLog.name === 'ManagerDeployed');
-  const managerAddress = event?.parsedLog.args._manager as string;
-
-  return {
-    managerAddress,
-  };
 }
 
 export interface LaunchTokenParams {
@@ -177,101 +133,5 @@ export const getLaunchTokenParams = async (
     coinData,
     flaunchParams,
     fee,
-  };
-}
-
-export const executeLaunchToken = async (
-  zapContractAddress: string,
-  rpcProvider: ethers.Provider,
-  signer: ethers.Signer,
-  txParams: LaunchTokenTxParams,
-) => {
-  const writeContract = new ethers.Contract(zapContractAddress, ZapContractABI.abi, signer);
-  const readContract = new ethers.Contract(zapContractAddress, ZapContractABI.abi, rpcProvider);
-
-  //-- estimate gas for the transaction
-  const estimatedGas = await writeContract.flaunch.estimateGas(...txParams.flaunchParams, { value: txParams.fee });
-
-  //-- write operation: launch the token
-  const tx: ethers.TransactionResponse = await writeContract.flaunch(...txParams.flaunchParams, {
-    value: txParams.fee,
-    gasLimit: estimatedGas,
-  });
-
-  const receipt = await tx.wait();
-
-  //-- read operations: get contract addresses and token info
-  const flaunch = await readContract.flaunchContract() as string;
-
-  //-- read token id from Transfer event in the flaunch contract
-  const flaunchInterface = new ethers.Interface(FlaunchABI.abi);
-  const event = parseLogs(receipt, flaunchInterface).find(
-    (log) => log.address == flaunch.toLowerCase() && log.parsedLog.name === 'Transfer'
-  );
-
-  const tokenId = event?.parsedLog.args.tokenId as bigint;
-
-  //-- get the memecoin from the flaunch contract (read operation)
-  const flaunchContract = new ethers.Contract(flaunch, FlaunchABI.abi, rpcProvider);
-
-  const memecoin = await flaunchContract.memecoin(tokenId) as string;
-
-  return {
-    memecoin,
-    flaunch,
-    tokenId,
-  };
-}
-
-export const launchToken = async (
-  zapContractAddress: string, //-- get from chain
-  addressFeeSplitManagerImplementationContract: string, //-- get from chain
-  rpcProvider: ethers.Provider, //-- RPC provider for read operations
-  signer: ethers.Signer, //-- signer for write operations
-  params: LaunchTokenParams,
-) => {
-  const txParams = await getLaunchTokenParams(
-    zapContractAddress,
-    addressFeeSplitManagerImplementationContract,
-    rpcProvider,
-    params,
-  );
-
-  return executeLaunchToken(zapContractAddress, rpcProvider, signer, txParams);
-}
-
-export const launchTokenToGroup = async (
-  zapContractAddress: string, //-- get from chain
-  addressFeeSplitManagerImplementationContract: string, //-- get from chain
-  rpcProvider: ethers.Provider, //-- RPC provider for read operations
-  signer: ethers.Signer, //-- signer for write operations
-  params: LaunchTokenParams,
-  groupAddress: string, //-- the treasury manager address returned from the createGroup function
-) => {
-  //-- first launch the token as normal
-  const { memecoin, flaunch, tokenId } = await launchToken(
-    zapContractAddress,
-    addressFeeSplitManagerImplementationContract,
-    rpcProvider,
-    signer,
-    { ...params, feeSplit: undefined },//-- no fee split when launching the token to a group
-  );
-  
-  //-- read operation: get creator from flaunch contract
-  const flaunchContract = new ethers.Contract(flaunch, FlaunchABI.abi, rpcProvider);
-  const creator = await flaunchContract.ownerOf(tokenId) as string;
-
-  //-- write operation: approve the transfer of NFT to the group
-  const erc721Contract = new ethers.Contract(memecoin, IERC721ABI.abi, signer);
-  const approveTx = await erc721Contract.approve(groupAddress, 1);
-  await approveTx.wait();
-
-  //-- write operation: call the group to deposit the NFT
-  const groupContract = new ethers.Contract(groupAddress, PositionManagerABI.abi, signer);
-  const depositTx = await groupContract.deposit([flaunch, tokenId], creator, '');
-  await depositTx.wait();
-
-  return {
-    memecoin,
   };
 }
