b546f8e156825f201205b338eb461364483fbecc
Nam Nguyen <namhoang.td09@gmail.com>
2025-11-24 18:04:07 +0700

    feat: update success modal

---
+++ lib/components/features/create-coin/SuccessModal.tsx
@@ -0,0 +1,41 @@
+import { Button, modal } from '$lib/components/core';
+
+export function SuccessModal({
+  title,
+  description,
+  txUrl
+}: {
+  title: string;
+  description: string;
+  txUrl?: string;
+}) {
+  return (
+    <div className="p-4 space-y-4 w-[340px]">
+      <div className="size-[56px] flex justify-center items-center rounded-full bg-success-500/16">
+        <i className="icon-done text-success-500" />
+      </div>
+      <div className="space-y-2">
+        <p className="text-lg">{title}</p>
+        <p className="text-sm text-secondary break-words">{description}</p>
+      </div>
+      <div className="grid grid-cols-2 gap-2">
+        {txUrl && (
+          <Button
+            variant="tertiary"
+            onClick={() => window.open(txUrl, '_blank')}
+            className="w-full"
+          >
+            View Txn.
+          </Button>)
+        }
+        <Button
+          variant="secondary"
+          onClick={() => modal.close()}
+          className="w-full"
+        >
+          Done
+        </Button>
+      </div>
+    </div>
+  );
+}
