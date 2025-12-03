e49fba53c2b133d183365924936cdb821b2dda33
Chris Nguyen <chris.dungnguyen@gmail.com>
2025-12-03 18:40:45 +0700

    feat: implement token page

---
+++ lib/components/core/progess/radial.tsx
@@ -0,0 +1,50 @@
+'use client';
+import { twMerge } from 'tailwind-merge';
+
+export function RadialProgress({
+  size = 'size-10',
+  color,
+  value = 0,
+  label,
+}: {
+  size?: string;
+  color: string;
+  value?: number;
+  label?: string;
+}) {
+  return (
+    <div className={twMerge('relative', size)}>
+      <div className={twMerge('relative aspect-square rotate-[-90deg]', size)}>
+        <svg className="w-full h-full" viewBox="0 0 100 100">
+          <circle
+            className="text-quaternary stroke-current"
+            stroke-width="4"
+            cx="50"
+            cy="50"
+            r="40"
+            fill="transparent"
+          ></circle>
+
+          <circle
+            className={twMerge('stroke-current transition-all duration-700', color)}
+            stroke-width="16"
+            stroke-linecap="round"
+            cx="50"
+            cy="50"
+            r="40"
+            fill="transparent"
+            stroke-dasharray="251.2"
+            style={{ 'stroke-dashoffset': `calc(251.2 - (251.2 * ${value}) / 100)` }}
+          ></circle>
+        </svg>
+      </div>
+      {label && (
+        <div className="absolute inset-0">
+          <div className="flex items-center justify-center w-full h-full text-sm">
+            <p>{label}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
