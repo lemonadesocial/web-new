3b187b1f7c218796cba300f59e5a8fc7a1da7278
Chris Nguyen <chrisnguyen@Macmini.lan>
2025-12-10 09:17:32 +0700

    feat: import abis

--- lib/utils/crypto.ts
+++ lib/utils/crypto.ts
@@ -11,6 +11,7 @@ import LemonheadNFT from '$lib/abis/LemonheadNFT.json';
 import LemonadePassport from '$lib/abis/LemonadePassport.json';
 import { TRPCClientError } from '@trpc/client';
 import ZugramaPassport from '$lib/abis/ZuGramaPassport.json';
+import MusicNft from '$lib/abis/MusicNft.json';
 
 export const ERC20Contract = new ethers.Contract(ethers.ZeroAddress, new ethers.Interface(ERC20));
 export const ERC721Contract = new ethers.Contract(ethers.ZeroAddress, new ethers.Interface(ERC721));
