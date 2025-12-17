4d8a639826f152e5a834840fb1c0da269b257c38
Chris Nguyen <chrisnguyen@Macmini.lan>
2025-12-17 16:24:49 +0700

    feat: checking layout

--- app/[domain]/(blank)/community/page.tsx
+++ app/[domain]/(blank)/community/page.tsx
@@ -46,21 +46,5 @@ export default async function Page({ params }: Props) {
 
   if (!space) return notFound();
 
-  // const { subSpaces, spaceTags } = await prefetchData(space);
-
   return <Community initData={{ space }} />;
 }
-
-// const prefetchData = async (space: Space) => {
-//   const client = getClient();
-//
-//   const [subSpaces, spaceTags] = await Promise.all([
-//     client.query({ query: GetSubSpacesDocument, variables: { id: space._id } }),
-//     client.query({ query: GetSpaceTagsDocument, variables: { space: space._id } }),
-//   ]);
-//
-//   return {
-//     subSpaces: (subSpaces.data?.getSubSpaces || []) as PublicSpace[],
-//     spaceTags: (spaceTags.data?.listSpaceTags || []) as SpaceTag[],
-//   };
-// };
