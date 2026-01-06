3041588682a6a1b026a85e90051dfea8e09d89af
Chris Nguyen <chrisnguyen@Macmini.lan>
2026-01-06 20:03:32 +0700

    feat: remove test

--- lib/components/features/community/widgets/WidgetContainer.tsx
+++ lib/components/features/community/widgets/WidgetContainer.tsx
@@ -7,15 +7,14 @@ import { defaultPassportConfig } from '../../theme-builder/passports';
 export function WidgetContainer({ space }: { space: Space }) {
   const [state, dispatch] = useTheme();
 
-  // React.useEffect(() => {
-  //   if (space.theme_name) {
-  //     const config = defaultPassportConfig['zugrama'] || defaultPassportConfig[space.theme_name as string];
-  //     const main = document.getElementsByTagName('main')?.[0];
-  //     main.setAttribute('style', `background: url(${config.template.image}); background-size: cover`);
-  //     dispatch({ type: ThemeBuilderActionKind.select_template, payload: { ...config } });
-  //   }
-  // }, [space.theme_name]);
-
+  React.useEffect(() => {
+    if (space.theme_name) {
+      const config = defaultPassportConfig[space.theme_name as string];
+      const main = document.getElementsByTagName('main')?.[0];
+      main.setAttribute('style', `background: url(${config.template.image}); background-size: cover`);
+      dispatch({ type: ThemeBuilderActionKind.select_template, payload: { ...config } });
+    }
+  }, [space.theme_name]);
 
   return (
     <div data-coin-template className="grid grid-cols-1 lg:grid-cols-4 gap-8">
