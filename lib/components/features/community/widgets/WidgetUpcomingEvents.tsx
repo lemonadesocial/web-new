9acbf185b4efe6e60cb0dbfab634e62c9282ec51
Chris Nguyen <chrisnguyen@Macmini.lan>
2026-01-07 22:07:23 +0700

    feat: update empty upcoming events widget

--- lib/components/features/community/widgets/WidgetUpcomingEvents.tsx
+++ lib/components/features/community/widgets/WidgetUpcomingEvents.tsx
@@ -1,12 +1,25 @@
 'use client';
+import React from 'react';
+
 import { useRouter } from 'next/navigation';
 
 import { Avatar, Card } from '$lib/components/core';
-import { Event, GetSpaceEventsDocument, Space } from '$lib/graphql/generated/backend/graphql';
-import { useQuery } from '$lib/graphql/request';
+import {
+  Event,
+  GetSpaceEventsDocument,
+  Space,
+  FollowSpaceDocument,
+  UnfollowSpaceDocument,
+} from '$lib/graphql/generated/backend/graphql';
+import { useMutation, useQuery } from '$lib/graphql/request';
 import { generateUrl } from '$lib/utils/cnd';
 import { formatWithTimezone } from '$lib/utils/date';
 import { WidgetContent } from './WidgetContent';
+import { useAtom } from 'jotai';
+import { useSignIn } from '$lib/hooks/useSignIn';
+
+import { sessionAtom } from '$lib/jotai';
+import { useMe } from '$lib/hooks/useMe';
 
 interface Props {
   space: Space;
@@ -31,6 +44,8 @@ export function WidgetUpcomingEvents({ space }: Props) {
   if (loading) return null;
   const upcomingEvents = (data?.getEvents || []) as Event[];
 
+  if (!upcomingEvents.length) return <EmptyUpcomingEvents space={space} />;
+
   return (
     <WidgetContent
       space={space}
@@ -44,6 +59,7 @@ export function WidgetUpcomingEvents({ space }: Props) {
           <h3 className="text-2xl font-semibold">{upcomingEvents.length}</h3>
           <p className="text-tertiary">Upcoming</p>
         </div>
+
         {upcomingEvents.map((item) => (
           <Card.Root
             key={item._id}
@@ -65,6 +81,88 @@ export function WidgetUpcomingEvents({ space }: Props) {
             </Card.Content>
           </Card.Root>
         ))}
+      </div>
+    </WidgetContent>
+  );
+}
+
+function EmptyUpcomingEvents({ space }: { space: Space }) {
+  const router = useRouter();
+
+  const [session] = useAtom(sessionAtom);
+  const me = useMe();
+  const signIn = useSignIn();
+
+  const [follow] = useMutation(FollowSpaceDocument, {
+    onComplete: (client) => {
+      client.writeFragment({ id: `Space:${space?._id}`, data: { followed: true } });
+    },
+  });
+  const [unfollow] = useMutation(UnfollowSpaceDocument, {
+    onComplete: (client) => {
+      client.writeFragment({ id: `Space:${space?._id}`, data: { followed: false } });
+    },
+  });
+
+  const handleSubscribe = () => {
+    if (!session) {
+      // need to login to subscribe
+      signIn();
+      return;
+    }
+
+    const variables = { space: space?._id };
+    if (space?.followed) unfollow({ variables });
+    else follow({ variables });
+  };
+
+  const canSubscribe = ![space?.creator, ...(space?.admins?.map((p) => p._id) || [])].includes(me?._id);
+
+  return (
+    <WidgetContent
+      space={space}
+      canSubscribe={false}
+      title="Events"
+      className="col-span-2"
+      onClick={() => router.push(`/s/${space.slug || space._id}/events`)}
+    >
+      <div className="p-6 flex gap-6 w-full">
+        <div className="flex-1 flex flex-col justify-between">
+          <div>
+            <h3 className="text-2xl font-semibold">0</h3>
+            <p className="text-tertiary">Upcoming</p>
+          </div>
+          <div className="text-tertiary">
+            <p>Click to view</p>
+            <p> 6 past events</p>
+          </div>
+        </div>
+
+        <div className="flex-1 flex flex-col gap-5 ">
+          <Card.Root
+            className="bg-(--btn-tertiary)"
+            onClick={
+              canSubscribe
+                ? (e) => {
+                    e.stopPropagation();
+                    handleSubscribe();
+                  }
+                : undefined
+            }
+          >
+            <Card.Content className="flex flex-col justify-center gap-3">
+              <i className="icon-user-plus" />
+              <p>Subscribe</p>
+            </Card.Content>
+          </Card.Root>
+
+          <Card.Root className="bg-(--btn-tertiary)">
+            <Card.Content className="flex flex-col justify-center gap-3">
+              <i className="icon-user-plus" />
+              <p>Submit Event</p>
+            </Card.Content>
+          </Card.Root>
+        </div>
       </div>
     </WidgetContent>
   );
