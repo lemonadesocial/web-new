c3771a09eb0df84d4043a4fa438905eda47f4982
Chris Nguyen <chrisnguyen@Macmini.lan>
2026-01-07 20:43:32 +0700

    temp: hide tokens - explore - creat coin

--- app/[domain]/(default)/tokens/page.tsx
+++ app/[domain]/(default)/tokens/page.tsx
@@ -1,5 +1,9 @@
 import { Tokens } from '$lib/components/features/tokens/Tokens';
+import { notFound } from 'next/navigation';
 
+// FIXME: temp hide
 export default function Page() {
+  if (process.env.APP_ENV === 'production') return notFound();
+
   return <Tokens />;
 }
