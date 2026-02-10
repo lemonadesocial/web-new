313a047239dda34f6e040529e1dd8068355478b9
Chris Nguyen <chrisnguyen@Macmini.lan>
2026-02-10 16:11:49 +0700

    feat: checking sidebar ui

--- lib/components/layouts/sidebar.tsx
+++ lib/components/layouts/sidebar.tsx
@@ -21,6 +21,7 @@ import { useLogOut } from '$lib/hooks/useLogout';
 import { ProfilePane } from '../features/pane';
 import { AIChatActionKind, useAIChat } from '../features/ai/provider';
 import { aiChat } from '../features/ai/AIChatContainer';
+import { match } from 'ts-pattern';
 
 const Sidebar = () => {
   const pathname = usePathname();
@@ -66,6 +67,8 @@ const Sidebar = () => {
     router.replace(path);
     aiChat.close();
   };
+
+  return <Container>content</Container>;
 
   return (
     <>
@@ -427,6 +430,254 @@ const Sidebar = () => {
     </>
   );
 };
+
+function Container({ children }: React.PropsWithChildren) {
+  const pathname = usePathname();
+  const [toggle, setToggle] = React.useState<'mini' | 'hide' | 'open'>('mini');
+  const router = useRouter();
+  const [_state, dispatch] = useAIChat();
+
+  const me = useMe();
+  const { account } = useAccount();
+
+  const logOut = useLogOut();
+  const signIn = useSignIn();
+
+  const mainMenu = useMemo(() => {
+    const menu = [
+      { icon: 'icon-home', path: '/', label: 'Home' },
+      { icon: 'icon-storefront-outline', path: '/lemonade-stand', label: 'Lemonade Stand' },
+      { icon: 'icon-community', path: '/communities', label: 'Community Hubs' },
+
+      // { icon: 'icon-swipe', path: '/swipe', label: 'Swipe & Match' },  // FIXME: add back when lemonheads  are live
+      // { icon: 'icon-trophy', path: '/leaderboard', label: 'Leaderboard' },
+      // { icon: 'icon-passport', path: '/lemonheads', label: 'LemonHeads Zone' },
+    ];
+
+    return menu;
+  }, []);
+
+  const exploreMenu = useMemo(() => {
+    const menu = [
+      { icon: 'icon-explore', path: '/explore', label: 'Discover' },
+      { icon: 'icon-token', path: '/tokens', label: 'Coins' },
+      { icon: 'icon-newspaper', path: '/timelines', label: 'Newsfeed' },
+    ];
+    return menu;
+  }, []);
+
+  const isActive = (item: { path: string }) =>
+    pathname === item.path || (item.path.startsWith('/lemonheads') && pathname.includes(item.path));
+
+  const handleNavigate = (path: string) => {
+    dispatch({ type: AIChatActionKind.reset });
+    router.replace(path);
+    aiChat.close();
+  };
+
+  return (
+    <>
+      <div className="md:hidden fixed z-50 top-0 bg-background left-0 right-0 p-2.5">
+        <button
+          className="text-tertiary p-2.5 flex justify-center cursor-pointer"
+          onClick={() => setToggle((prev) => (prev !== 'hide' ? 'hide' : 'open'))}
+        >
+          <i className="icon-left-panel-close-outline size-5" />
+        </button>
+      </div>
+      <div
+        className={clsx(
+          'relative bg-overlay-primary transition-all duration-300 h-screen text-tertiary border-r max-sm:hidden z-10',
+          toggle === 'open' && 'w-64',
+          toggle === 'mini' && 'w-16',
+        )}
+      >
+        <div className="flex flex-col h-full">
+          <div className="flex-1 flex flex-col overflow-hidden">
+            <div className="p-3 flex items-center justify-between">
+              <div className="group">
+                <div
+                  className={clsx('p-2 flex items-center justify-center', toggle === 'mini' && 'group-hover:hidden')}
+                >
+                  <i className="icon-lemonade-logo text-warning-300 size-6" />
+                </div>
+                {toggle === 'mini' && (
+                  <button
+                    className="p-2.5 items-center justify-center cursor-pointer hidden group-hover:flex"
+                    onClick={() => setToggle((prev) => (prev === 'mini' ? 'open' : 'mini'))}
+                  >
+                    <i className="icon-left-panel-close-outline size-5" />
+                  </button>
+                )}
+              </div>
+
+              {toggle === 'open' && (
+                <button
+                  className="p-2.5 flex items-center justify-center cursor-pointer"
+                  onClick={() => setToggle((prev) => (prev === 'mini' ? 'open' : 'mini'))}
+                >
+                  <i className="icon-left-panel-close-outline size-5" />
+                </button>
+              )}
+            </div>
+
+            <div className="px-3 pb-3 flex flex-col gap-1">
+              {mainMenu.map((item) => (
+                <div
+                  key={item.path}
+                  className={clsx(
+                    'cursor-pointer text-secondary p-2.5 flex gap-2.5 items-center hover:bg-(--btn-tertiary) rounded-sm',
+                    isActive(item) && 'bg-(--btn-tertiary)',
+                  )}
+                  onClick={() => handleNavigate(item.path)}
+                >
+                  <i className={twMerge('size-5 aspect-square', item.icon)} />
+                  {toggle === 'open' && <span className="text-sm whitespace-nowrap">{item.label}</span>}
+                </div>
+              ))}
+
+              <div
+                className={clsx(
+                  'cursor-pointer text-secondary p-2.5 flex gap-2.5 items-center hover:bg-(--btn-tertiary) rounded-sm',
+                )}
+                onClick={() => modal.open(CreatingModal)}
+              >
+                <i className="size-5 icon-plus aspect-square" />
+                {toggle === 'open' && <span className="text-sm">Create</span>}
+              </div>
+            </div>
+
+            <div className="px-3 pb-3 flex flex-col gap-1">
+              <div className={clsx('text-tertiary text-sm p-2.5 pb-1.5', toggle === 'mini' && 'invisible')}>
+                <p>Explore</p>
+              </div>
+              {exploreMenu.map((item) => (
+                <div
+                  key={item.path}
+                  className={clsx(
+                    'cursor-pointer text-secondary p-2.5 flex gap-2.5 items-center hover:bg-(--btn-tertiary) rounded-sm',
+                    isActive(item) && 'bg-(--btn-tertiary)',
+                  )}
+                  onClick={() => handleNavigate(item.path)}
+                >
+                  <i className={twMerge('size-5 aspect-square', item.icon)} />
+                  {toggle === 'open' && <span className="text-sm whitespace-nowrap">{item.label}</span>}
+                </div>
+              ))}
+            </div>
+
+            <div className={clsx('flex-1 pb-2 overflow-hidden', toggle === 'mini' && 'hidden')}>
+              <Divider className="h-1 w-full" />
+              {toggle === 'open' && <SectionEvents handleNavigate={(path) => handleNavigate(path)} />}
+            </div>
+          </div>
+
+          <div className="border-t p-3 pt-4 flex flex-col gap-3">
+            <Card.Root
+              className={clsx(
+                'border-none hover:bg-(--btn-tertiary)',
+                toggle ? 'bg-(--btn-tertiary)' : 'bg-transparent',
+              )}
+            >
+              <Card.Content className={clsx('flex justify-between items-center', toggle ? 'px-3 py-2 gap-3' : 'p-2.5')}>
+                {match(toggle)
+                  .with('open', () => (
+                    <>
+                      <div>
+                        <p className="text-sm">Rewards</p>
+                        <p className="text-quaternary text-xs">Earn credits for your hubs</p>
+                      </div>
+                      <div className="p-2 bg-warning-600 rounded-full w-[30px] h-[30px] aspect-square flex items-center justify-center">
+                        <i className="icon-gift-line w-4 h-4" />
+                      </div>
+                    </>
+                  ))
+                  .with('mini', () => <i className="icon-gift-line size-5 aspect-square" />)
+                  .otherwise(() => null)}
+              </Card.Content>
+            </Card.Root>
+
+            <Card.Root
+              className={clsx(
+                'border-none hover:bg-(--btn-tertiary)',
+                toggle ? 'bg-(--btn-tertiary)' : 'bg-transparent',
+              )}
+            >
+              <Card.Content className={clsx('flex justify-between items-center', toggle ? 'px-3 py-2 gap-3' : 'p-2.5')}>
+                {match(toggle)
+                  .with('open', () => (
+                    <>
+                      <div>
+                        <p className="text-sm">Upgrade to Pro</p>
+                        <p className="text-quaternary text-xs">Unlock more benefits</p>
+                      </div>
+                      <div className="p-2 bg-alert-500 rounded-full w-[30px] h-[30px] aspect-square flex items-center justify-center">
+                        <i className="icon-flash w-4 h-4" />
+                      </div>
+                    </>
+                  ))
+                  .with('mini', () => <i className="icon-flash size-5 aspect-square" />)
+                  .otherwise(() => null)}
+              </Card.Content>
+            </Card.Root>
+
+            {me || account ? (
+              <Menu.Root strategy="absolute" placement="top-start">
+                <Menu.Trigger>
+                  {() => (
+                    <div className="flex gap-2 items-center p-2">
+                      <img src={userAvatar(me)} className="rounded-full border aspect-square w-6 h-6" />
+                      {toggle === 'open' && (
+                        <div className="flex flex-col">
+                          <p className="text-sm font-medium text-secondary">
+                            {me?.username || me?.display_name || me?.name}
+                          </p>
+                        </div>
+                      )}
+                    </div>
+                  )}
+                </Menu.Trigger>
+                <Menu.Content className="p-0 min-w-[228px] backdrop-blur-md!">
+                  {({ toggle }) => (
+                    <>
+                      <div className="p-1">
+                        <MenuItem
+                          title="Edit Profile"
+                          onClick={() => drawer.open(ProfilePane, { dismissible: false })}
+                        />
+                        <MenuItem title="Settings" onClick={() => router.push('/settings')} />
+                        <MenuItem
+                          title="Sign Out"
+                          onClick={async () => {
+                            toggle();
+                            logOut();
+                          }}
+                        />
+                      </div>
+                    </>
+                  )}
+                </Menu.Content>
+              </Menu.Root>
+            ) : (
+              <div
+                className="flex gap-2 items-center p-2 cursor-pointer hover:bg-(--btn-tertiary) rounded-sm"
+                onClick={() => signIn()}
+              >
+                <img src={userAvatar(me)} className="rounded-full border aspect-square w-6 h-6" />
+                {toggle === 'mini' && (
+                  <div className="flex flex-col">
+                    <p className="text-sm font-medium text-secondary">Login</p>
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
+          {children}
+        </div>
+      </div>
+    </>
+  );
+}
 
 const actions = {
   event: { icon: 'icon-ticket text-accent-400', title: 'Event', subtitle: 'Virtual & IRL' },
