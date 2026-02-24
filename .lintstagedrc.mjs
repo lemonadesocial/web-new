d2f6d844d3de986f5092fe80fffc071030a6f121
Harley Trung <harley@coderpush.com>
2026-02-24 23:11:06 +0700

    chore: limit lint-staged prettier to staged files

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,8 +1,10 @@
 import path from 'path';
 
+const toRelativePaths = (filenames) => filenames.map((f) => path.relative(process.cwd(), f));
+const buildPrettierCommand = (filenames) => `prettier --write ${toRelativePaths(filenames).join(' ')}`;
 const buildEslintCommand = (filenames) =>
-  `prettier --write && next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
+  `next lint --fix --file ${toRelativePaths(filenames).join(' --file ')}`;
 
 export default {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{js,jsx,ts,tsx}': [buildPrettierCommand, buildEslintCommand],
 };
