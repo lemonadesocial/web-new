95139f09a83064338a7ce8a8319da6d0b6e529a5
Chris Nguyen <chris.dungnguyen@gmail.com>
2026-03-27 15:40:09 +0700

    fix: layout save changes CTA bar

--- lib/components/features/upgrade-to-pro/CommunityDetail.tsx
+++ lib/components/features/upgrade-to-pro/CommunityDetail.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from '../theme-builder/provider';
 export function CommunityDetail({ space }: { space: Space }) {
   return (
     <ThemeProvider themeData={space.theme_data}>
-      <div className="**:data-submit-content:top-0! **:data-submit-content:px-4! **:data-form-content:px-4">
+      <div className="**:data-submit-content:fixed **:data-submit-content:left-70 **:data-submit-content:right-0 **:data-submit-content:top-0! **:data-submit-content:px-4! **:data-form-content:px-4">
         <CommunityDetailForm space={space} />
       </div>
     </ThemeProvider>
