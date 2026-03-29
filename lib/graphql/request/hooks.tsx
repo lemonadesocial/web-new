a9a5f54b51f12a88f53cf27ba61524f1f942ed4a
Chris Nguyen <chris.dungnguyen@gmail.com>
2026-03-29 08:54:46 +0700

    fix: correct type check

--- lib/graphql/request/hooks.tsx
+++ lib/graphql/request/hooks.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import { TypedDocumentNode } from '@graphql-typed-document-node/core';
-import { isEqual } from 'lodash';
 
 import { MutationOptions, QueryOptions } from './type';
 import { GraphqlClient } from './client';
 import { defaultClient } from './instances';
+
+function isEqual(a: any, b: any): boolean {
+  if (a === b) return true;
+  if (a && b && typeof a === 'object' && typeof b === 'object') {
+    if (a.constructor !== b.constructor) return false;
+    let length, i;
+    if (Array.isArray(a)) {
+      length = a.length;
+      if (length != b.length) return false;
+      for (i = length; i-- !== 0; ) if (!isEqual(a[i], b[i])) return false;
+      return true;
+    }
+    if (a.valueOf !== Object.prototype.valueOf) return a.valueOf() === b.valueOf();
+    if (a.toString !== Object.prototype.toString) return a.toString() === b.toString();
+    const keys = Object.keys(a);
+    length = keys.length;
+    if (length !== Object.keys(b).length) return false;
+    for (i = length; i-- !== 0; ) if (!Object.prototype.hasOwnProperty.call(b, keys[i])) return false;
+    for (i = length; i-- !== 0; ) {
+      const key = keys[i];
+      if (!isEqual(a[key], b[key])) return false;
+    }
+    return true;
+  }
+  return a !== a && b !== b;
+}
 
 export function useQuery<T, V extends object>(
   query: TypedDocumentNode<T, V>,
@@ -63,10 +88,10 @@ export function useQuery<T, V extends object>(
       setLoading(true);
       const { data: newData } = await client.query({
         query,
-        variables: { ...variables, ...params.variables },
+        variables: { ...(variables as any), ...(params.variables as any) },
         fetchPolicy: 'network-only',
       });
-      const latestData = params.updateQuery?.(data as T, newData) as T;
+      const latestData = params.updateQuery?.(data as T, newData as T) as T;
 
       setData((prev) => {
         if (isEqual(prev, latestData)) return prev;
