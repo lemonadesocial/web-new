554bc7079e1ee512f98b0f04fb0ca13a1bf00986
Chris Nguyen <chris.dungnguyen@gmail.com>
2026-03-31 07:33:28 +0700

    perf: refactor GraphqlClient to support parallel requests

--- lib/graphql/request/client.ts
+++ lib/graphql/request/client.ts
@@ -44,7 +44,6 @@ export class GraphqlClient {
   private client: GraphQLClient;
   private cache?: InMemoryCache;
   private queue: QueryRequest<any, any>[] = [];
-  private processing: boolean = false;
   customHeader: Record<string, string> = {};
 
   constructor({ url, cache, options = {} }: { url: string; cache?: InMemoryCache; options?: RequestConfig }) {
@@ -110,23 +109,18 @@ export class GraphqlClient {
     });
   }
 
-  private async processQueue() {
-    if (this.processing || this.queue.length === 0) return;
+  private processQueue() {
+    while (this.queue.length > 0) {
+      const request = this.queue.shift()!;
+      this.executeRequestWrapper(request);
+    }
+  }
 
-    this.processing = true;
-    const request = this.queue.shift()!;
-
+  private async executeRequestWrapper(request: QueryRequest<any, any>) {
     try {
-      if (request.headers) {
-        this.client.setHeaders(request.headers);
-      }
-
       await this.executeRequest(request);
     } catch (error) {
       this.handleRequestError(request, error);
-    } finally {
-      this.processing = false;
-      this.processQueue();
     }
   }
 
@@ -136,7 +130,7 @@ export class GraphqlClient {
       return;
     }
 
-    const { fetchPolicy, initData } = request;
+    const { fetchPolicy, initData, headers } = request;
 
     if (fetchPolicy === 'network-only') {
       await this.executeNetworkRequest(request);
@@ -157,15 +151,15 @@ export class GraphqlClient {
 
     let result = initData;
     if (!result) {
-      result = await this.client.request(request.query, request.variables);
+      result = await this.client.request(request.query, request.variables, headers as HeadersInit);
     }
     this.cache?.normalizeAndStore(request.query, request.variables, result);
     request.resolve({ data: result, error: null });
   }
 
   private async executeNetworkRequest(request: QueryRequest<any, any>) {
-    const { query, variables } = request;
-    const result = await this.client.request(query, variables);
+    const { query, variables, headers } = request;
+    const result = await this.client.request(query, variables, headers as HeadersInit);
 
     this.cache?.normalizeAndStore(query, variables, result);
     request.resolve({ data: result, error: null });
