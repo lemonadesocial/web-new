c7027347cbff84e6c27529bb2abc85afd09de839
Chris Nguyen <chris.dungnguyen@gmail.com>
2026-04-13 19:24:45 +0700

    feat(page-builder): enable full-width design and set centered content as system default

--- lib/components/features/ai/manage/craft/CraftableEventSections.tsx
+++ lib/components/features/ai/manage/craft/CraftableEventSections.tsx
@@ -63,7 +63,7 @@ export function CraftableEventSections({
       ROOT: {
         type: { resolvedName: 'Container' },
         isCanvas: true,
-        props: { className: 'w-full' },
+        props: { className: 'w-full', centered: true },
         nodes: ['main-grid'],
         linkedNodes: {},
         parent: null,
